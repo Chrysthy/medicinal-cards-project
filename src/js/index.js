256bda74fb6a7f3565d6712de8fa214088148c32
Chrystine Martins <chrystine.martins@outlook.com>
2024-10-13 12:59:06 -0300

    remove comments

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,3 @@
-/*
-   OBJETIVO - quando clicar na listagem temos que esconder o cartão aberto e mostrar o cartão correspondente ao que foi selecionado na listagem
-       PASSO 1 - precisamos criar uma variável no JS pra trabalhar com a listagem
-       PASSO 2 - Identificar o evento de clique no elemento da listagem
-       PASSO 3 - remover a classe aberto só do cartão que tá aberto
-       PASSO 4 - ao clicar em um pokedev da lista pegamos o id desse pra saber qual cartão abrir
-       PASSO 5 - remover a classe ativo que marca o selecionado na listagem
-       PASSO 6 - adicinar a classe ativo selecionado na listagem
-*/
-
 const listaSelecaoMedicinais = document.querySelectorAll('.medicinais');
 
 listaSelecaoMedicinais.forEach(item => {
