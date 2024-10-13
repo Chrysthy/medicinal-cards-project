2b052628bb16130c88ea60b9c6845f6eaebadf57
Chrystine Martins <chrystine.martins@outlook.com>
2024-10-13 11:23:27 -0300

    add classList remove and add

--- src/js/index.js
+++ src/js/index.js
@@ -15,18 +15,21 @@ listaSelecaoMedicinais.forEach(item => {
    item.addEventListener('click', () => {
 
       const cartaoAberto = document.querySelector(".aberto")
-      cartaoAberto.classList.remove(".aberto")
+      cartaoAberto.classList.remove("aberto")
 
       const idSelecionado = item.attributes.id.value;
 
       const idCartaoAbrir = "cartao-" + idSelecionado
 
       const cartaoAbrir = document.getElementById(idCartaoAbrir)
+      cartaoAbrir.classList.add("aberto")
 
 
+      const medicinaisAtivo = document.querySelector(".ativo")
+      medicinaisAtivo.classList.remove("ativo")
 
-
-
+      const medicinaisSelecionado = document.getElementById(idSelecionado)
+      medicinaisSelecionado.classList.add("ativo")
 
    });
 
