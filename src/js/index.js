e8c6fabd6ba6733ee7acea355e16e05e2d48f1ee
Chrystine Martins <chrystine.martins@outlook.com>
2024-10-13 10:54:36 -0300

    add const lista

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,13 @@
 /*
-   OBJETIVO - quando clicar no pokedev da listagem temos que esconder o cartão do pokedev aberto 
-   e mostrar o cartão correspondente ao que foi selecionado na listagem
-       PASSO 1 - precisamos criar uma variável no JS pra trabalhar com a listagem de pokedevs
+   OBJETIVO - quando clicar na listagem temos que esconder o cartão aberto e mostrar o cartão correspondente ao que foi selecionado na listagem
+       PASSO 1 - precisamos criar uma variável no JS pra trabalhar com a listagem
        PASSO 2 - Identificar o evento de clique no elemento da listagem
        PASSO 3 - remover a classe aberto só do cartão que tá aberto
-       PASSO 4 - ao clicar em um pokedev da lista pegamos o id desse pokedev pra saber qual 
-    cartão abrir
-       PASSO 5 - remover a classe ativo que marca o pokedev selecionado na listagem
-       PASSO 6 - adicinar a classe ativo no pokedev selecionado na listagem
+       PASSO 4 - ao clicar em um pokedev da lista pegamos o id desse pra saber qual cartão abrir
+       PASSO 5 - remover a classe ativo que marca o selecionado na listagem
+       PASSO 6 - adicinar a classe ativo selecionado na listagem
 */
 
-const listaSelecaoMedicinais = document.querySelectorAll('.medicinal');
-console.log(listaSelecaoMedicinais);
+const listaSelecaoMedicinais = document.querySelectorAll('.medicinais');
+
+
